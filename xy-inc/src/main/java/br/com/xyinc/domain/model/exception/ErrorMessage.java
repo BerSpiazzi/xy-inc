3a85822afc53f20a328d38cfb8fc009a2fa7b58d
Bernardo Bortot Spiazzi <bernardo.spiazzi@ciss.com.br>
2024-06-21 22:45:52 -0300

    create Exception Error Message

---
+++ xy-inc/src/main/java/br/com/xyinc/domain/model/exception/ErrorMessage.java
@@ -0,0 +1,23 @@
+package br.com.xyinc.domain.model.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private int status;
+    private String error;
+    private String path;
+    private List<String> errors;
+}
