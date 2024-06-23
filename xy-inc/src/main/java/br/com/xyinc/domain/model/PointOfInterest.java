6a64982cff035a63afa8ce35695388afec2d0c49
Bernardo Bortot Spiazzi <bernardo.spiazzi@ciss.com.br>
2024-06-23 13:33:22 -0300

    Alter class properties with wrong types validation

--- xy-inc/src/main/java/br/com/xyinc/domain/model/PointOfInterest.java
+++ xy-inc/src/main/java/br/com/xyinc/domain/model/PointOfInterest.java
@@ -7,27 +7,33 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString.Exclude;
 
 @Entity(name = "point_of_interest")
-public record PointOfInterest(
+@Getter
+@Setter
+public class PointOfInterest {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Exclude
-        long id,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Exclude
+    private long id;
 
-        @NotBlank(message = "O atributo 'nome' não pode ser vazio")
-        @Size(max = 50, message = "O atributo 'nome' deve ter no máximo 50 caracteres")
-        String nome,
+    @NotBlank(message = "O atributo 'nome' não pode ser vazio")
+    @Size(max = 50, message = "O atributo 'nome' deve ter no máximo 50 caracteres")
+    private String nome;
 
-        @NotNull(message = "O atributo 'coordenadaX' não pode ser nulo")
-        @Digits(integer = 10, fraction = 0, message = "O atributo 'coordenadaX' deve ser um número inteiro")
-        int coordenadaX,
+    @NotNull(message = "O atributo 'coordenadaX' não pode ser nulo")
+    @Digits(integer = 10, fraction = 0, message = "O atributo 'coordenadaX' deve ser um número inteiro")
+    @PositiveOrZero(message = "O atributo 'coordenadaX' deve ser um número inteiro positivo ou zero")
+    private Integer coordenadaX;
 
-        @NotNull(message = "O atributo 'coordenadaY' não pode ser nulo")
-        @Digits(integer = 10, fraction = 0, message = "O atributo 'coordenadaY' deve ser um número inteiro")
-        int coordenadaY) {
-
+    @NotNull(message = "O atributo 'coordenadaY' não pode ser nulo")
+    @Digits(integer = 10, fraction = 0, message = "O atributo 'coordenadaY' deve ser um número inteiro")
+    @PositiveOrZero(message = "O atributo 'coordenadaY' deve ser um número inteiro positivo ou zero")
+    private Integer coordenadaY;
 }
